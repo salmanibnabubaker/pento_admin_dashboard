4323e07eb77be63a3b8d47ceb7b9f2948129638e
salmanibnabubaker <vksalmansalmanpes@gmail.com>
2026-08-13 22:36:53 +0530

    login button in homepage added

--- app/page.tsx
+++ app/page.tsx
@@ -62,6 +62,21 @@ export default function Home() {
           >
             Documentation
           </a>
+
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="/login"
+          >
+            <Image
+              className="dark:invert h-[14px] w-4"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={14}
+            />
+            login
+          </a>
+          
         </div>
       </main>
     </div>
